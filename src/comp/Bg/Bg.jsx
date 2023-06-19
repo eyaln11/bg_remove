fc2e74b30bfd243d9c9386554b5ff401dafa9dc6
evh671 <eyalnahom@gmail.com>
2023-06-19 14:22:11 +0300

    Background Remover Project - 1s Commit

---
+++ src/comp/Bg/Bg.jsx
@@ -0,0 +1,17 @@
+import "./Bg.css";
+import close from "./../../images/close.png";
+
+function Bg() {
+  return (
+    <div className="bg_general_div">
+      <div className="bg_top_menu">
+        <img src={close} className="bg_close_btn" alt="close button" />
+        <div className="bg_top_menu_title">העלאת תמונה כדי להסיר את הרקע</div>
+        <div className="bg_top_menu_button">העלאת תמונה</div>
+        <div className="bg_top_menu_sub_title">פורמטים נתמכים: png, jpeg</div>
+      </div>
+    </div>
+  );
+}
+
+export default Bg;
