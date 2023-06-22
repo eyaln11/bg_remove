4368957387541bda87dd4decc4b1b76ae0c967b8
eyaln11 <eyalnahom@gmail.com>
2023-06-22 17:44:45 +0300

    Lesson 4 - Bgnobg Component + Input Color Style

--- src/comp/Bgnobg/Bgnobg.jsx
+++ src/comp/Bgnobg/Bgnobg.jsx
@@ -1,11 +1,39 @@
+import "./Bgnobg.css";
+import { useState, useRef } from "react";
+
 function Bgnobg() {
+  const [choosed_color, setchoosed_color] = useState(null);
+
+  const inputElement = useRef();
+
+  function choose_color_input() {
+    inputElement.current.click();
+  }
+
+  function choose_color_click(e) {
+    setchoosed_color(e.target.value);
+  }
+
   return (
     <div>
       <div className="Bgnobg_text">
         אל תשכחו להוריד את הקבצים. הם ימחקו אוטומטית כשתצא מהדף
       </div>
 
-      <input type="color" />
+      <div className="bg_color_div" onClick={choose_color_input}>
+        צבע רקע
+        <div
+          className="show_selected_color"
+          style={{ backgroundColor: choosed_color }}
+        ></div>
+      </div>
+
+      <input
+        type="color"
+        ref={inputElement}
+        className="color_input"
+        onInput={choose_color_click}
+      />
     </div>
   );
 }
